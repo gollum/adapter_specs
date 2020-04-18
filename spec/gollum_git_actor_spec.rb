b699007f2eae6beaf470af3a6a029c7d55a2d218
Daniel Senff <mail@danielsenff.de>
2020-04-18 13:34:56 +0200

    add time attribute to actor (#13)
    Add time attribute to actor

--- spec/gollum_git_actor_spec.rb
+++ spec/gollum_git_actor_spec.rb
@@ -4,9 +4,10 @@ describe Gollum::Git::Actor do
 
   subject(:actor) { Gollum::Git::Actor.new("Tom Werner", "tom@example.com") }
 
-  it "has accessors for name and email" do
+  it "has accessors for name, email and time" do
     expect(actor).to respond_to(:name)
     expect(actor).to respond_to(:email)
+    expect(actor).to respond_to(:time)
   end
 
   it "has an output method" do
