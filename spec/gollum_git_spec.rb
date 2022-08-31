ed27cd35228366531d9280121f1306f223deb300
Dawa Ometto <dawa.ometto@uni-leipzig.de>
2022-08-31 14:58:53 +0200

    Add specs related to new branch discover logic (#15)
    * Add spec for Repo#find_branch
    * Add spec for global_default_branch

---
+++ spec/gollum_git_spec.rb
@@ -0,0 +1,8 @@
+require 'spec_helper'
+
+describe Gollum::Git do
+    it "returns the global default branch" do
+        expect(Gollum::Git).to respond_to(:global_default_branch).with(0).arguments
+        expect(Gollum::Git.global_default_branch).to eq `git config --global init.defaultBranch`.chomp
+    end
+end
