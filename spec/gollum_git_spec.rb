129074a3540ffc5fe2ac10c6b570dc87950e9b2f
Dawa Ometto <dawa.ometto@uni-leipzig.de>
2022-08-31 15:16:59 +0200

    Update gollum_git_spec.rb
    String compare the output of global_default_branch.

--- spec/gollum_git_spec.rb
+++ spec/gollum_git_spec.rb
@@ -3,6 +3,6 @@ require 'spec_helper'
 describe Gollum::Git do
     it "returns the global default branch" do
         expect(Gollum::Git).to respond_to(:global_default_branch).with(0).arguments
-        expect(Gollum::Git.global_default_branch).to eq `git config --global init.defaultBranch`.chomp
+        expect(Gollum::Git.global_default_branch.to_s).to eq `git config --global init.defaultBranch`.chomp
     end
 end
