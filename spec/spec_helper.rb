402e76d0651da0372b9364a2e9e45e77d2a1f241
Dawa Ometto <dawa.ometto@uni-leipzig.de>
2023-08-29 20:06:23 +0200

    Add spec helper for creating a non-bare testrepo

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,14 @@ def fixture(name)
   File.join(File.dirname(__FILE__), 'fixtures', name)
 end
 
+def cloned_testpath(path, bare = false)
+  tmpdir = Dir.mktmpdir(self.class.name)
+  bare   = bare ? "--bare" : ""
+  redirect = Gem.win_platform? ? '' : '2>/dev/null'
+  %x{git clone #{bare} '#{path}' #{tmpdir} #{redirect}}
+  tmpdir
+end
+
 RSpec.configure do |config|
   config.expect_with :rspec do |c|
     c.syntax = :expect
